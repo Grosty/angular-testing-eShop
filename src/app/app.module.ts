2826c5ba0f264615744e69c72e98eabc855c44fc
Grosty <grostmaster@gmail.com>
2020-04-25 16:53:51 +0300

    15 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,8 +41,3 @@ import { ShippingComponent } from './shipping/shipping.component';
 export class AppModule { }
 
 
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
